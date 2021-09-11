58b6885c62050ce839e9a61d5d3be4c6b27e0e2b
elliotmartin <elliot.j.martin@outlook.com>
2021-09-10 22:47:03 -0700

    Fix edge orientation complete. G0 -> G1 in Thistlewaithe

--- solver.py
+++ solver.py
@@ -1,5 +1,8 @@
-from Rubik import *
-import queue
+import Rubik
+from collections import deque
+
+def prettify_cube_list(cube_list):
+    return [Rubik.perm_to_string_dict[m] for m in cube_list]
 
 def fix_edge_orientation(cube):
     """
@@ -9,5 +12,25 @@ def fix_edge_orientation(cube):
     :return: list of moves
     """
 
-    q = queue
-    visited = [cube]
+    visited = deque()
+    visited.append((cube,[]))
+    while visited:
+        next_cube = visited.popleft()
+        for m in Rubik.moves:
+            moves = next_cube[1] + [m]
+            new_cube = Rubik.perm_apply(m, next_cube[0])
+            if Rubik.check_edge_orientation((new_cube)):
+                return [moves[1]]
+            visited.append((new_cube, moves))
+    return None
+
+
+if __name__ == "__main__":
+    scramble = [Rubik.F, Rubik.R, Rubik.Bi]
+    scrambled = Rubik.multiple_perm_apply(scramble, Rubik.I)
+    #print(Rubik.check_edge_orientation(scrambled))
+    solution = fix_edge_orientation(Rubik.multiple_perm_apply(scramble, Rubik.I))
+    print(prettify_cube_list(solution))
+    #if type(solution) != str and type(solution):
+    #    print(prettify_cube_list(solution))
+
