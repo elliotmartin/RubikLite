553c6201f287dbb03cf1f4df604511af870adc9e
elliotmartin <elliot.j.martin@outlook.com>
2022-01-24 21:45:52 -0800

    begin adding canonical sequences and switch to htm

--- solver.py
+++ solver.py
@@ -6,7 +6,7 @@ import random
 #g1 - L,R,F,B,U2,D2
 #g2 - L,R,F2,B2,U2,D2
 #g3 - L2,R2,F2,B2,U2,D2
-G0_MOVES = Rubik.moves
+G0_MOVES = Rubik.h2m_moves
 G1_MOVES = [Rubik.L, Rubik.R, Rubik.F, Rubik.B, Rubik.perm_apply(Rubik.U, Rubik.U), Rubik.perm_apply(Rubik.D, Rubik.D)]
 G2_MOVES = [Rubik.L, Rubik.R, Rubik.perm_apply(Rubik.F, Rubik.F), Rubik.perm_apply(Rubik.B, Rubik.B),
             Rubik.perm_apply(Rubik.U, Rubik.U), Rubik.perm_apply(Rubik.D, Rubik.D)]
@@ -44,23 +44,54 @@ def prettify_cube_list(cube_list):
         raise Exception("Empty cube_list provided")
     return [Rubik.perm_to_string_dict_htm[m] for m in cube_list]
 
+# https://tomas.rokicki.com/g4g9.pdf
+# no turns of the same face can be adjacent
+# any pair of adjacent commuting single moves (such as U and D) are always given in a particular order
+# we use U before D, F before B, and R before L
+# if we verify the sequence is canonical every time a new move is added to it, we're only concerned about the last 2 moves
+def is_canonical(moves):
+    prev = moves[-2]
+    last = moves[-1]
 
+    if prev == last:
+        return False
+
+    if prev == Rubik.D and last == Rubik.U:
+        return False
+
+    if prev == Rubik.B and last == Rubik.F:
+        return False
+
+    if prev == Rubik.R and last == Rubik.L:
+        return False
+
+    return True
+
+
+# need to implement canonical sequences to make bfs more efficient. Currently it gets stuck processing a lot of move*4
 def cube_bfs(start, condition_function, valid_moves):
+    print("start bfs")
     max_depth = 0
     visited = deque()
     visited.append((start, []))
     while visited:
         next_cube = visited.popleft()
+        if max_depth > 1:
+            print(prettify_cube_list(next_cube[1]))
         for m in valid_moves:
             moves = next_cube[1] + [m]
+            if len(moves) > 2:
+                if not is_canonical(moves):
+                    continue
             if len(moves) > max_depth:
                 max_depth += 1
             new_cube = Rubik.perm_apply(m, next_cube[0])
             if condition_function(new_cube):
                 return moves
             visited.append((new_cube, moves))
-        if max_depth > 15:
-            break
+        #if max_depth > 5:
+         #   break
+    print([prettify_cube_list(v[1]) for v in visited])
     return None
 
 
@@ -70,24 +101,15 @@ def stage_one(cube):
 
 if __name__ == "__main__":
     # TODO: Turn these into tests
-    scramble = [Rubik.F, Rubik.R, Rubik.Bi, Rubik.U, Rubik.L, Rubik.F]
-    #scramble = []
-    #for i in range(20):
-    #    scramble.append(random.choice(Rubik.moves))
-    #print(prettify_cube_list(scramble))
-
+    scramble = [Rubik.F, Rubik.R, Rubik.Bi, Rubik.perm_apply(Rubik.U, Rubik.U), Rubik.L, Rubik.F]
     #scramble = [Rubik.F, Rubik.R, Rubik.Bi]
     scrambled = Rubik.multiple_perm_apply(scramble, Rubik.I)
-    #solution = cube_bfs(scrambled, Rubik.check_edge_orientation, G0_MOVES)
-    #print(solution)
-    #print("fix_eo: ", prettify_cube_list(solution))
-
 
     print(check_complete_g2(scrambled))
     g1_sol = cube_bfs(scrambled, Rubik.check_edge_orientation, G0_MOVES)
     print(prettify_cube_list(g1_sol))
-    #scrambled = Rubik.generate_cube()
+    scrambled = Rubik.generate_cube()
     solution = cube_bfs(Rubik.multiple_perm_apply(g1_sol, scrambled), check_complete_g2, G1_MOVES)
     print(solution)
-    print(prettify_cube_list(solution))
+    #print(prettify_cube_list(solution))
 
